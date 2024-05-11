e4c75c853d228b99b6b7ba344f33f984c829748d
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-05-11 18:41:11 -0300

    feat: create base signin screen

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -6,22 +7,48 @@
  */
 
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, TextInput} from 'react-native';
 import {Text} from './src/components/Text/Text';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
-import {Icon} from './src/components/Icon/Icon';
+import {Box} from './src/components/Box/Box';
+import {Button} from './src/components/Button/Button';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
-        <View style={{paddingHorizontal: 16}}>
-          <Text preset="headingMedium" italic>
-            Coffstack
+        <Box paddingHorizontal="s24">
+          <Text preset="headingLarge" marginBottom="s8">
+            Olá!
           </Text>
-          <Icon name="homeFill" />
-        </View>
+          <Text preset="paragraphLarge" marginBottom="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
+
+          <Box marginBottom="s20">
+            <TextInput
+              placeholder="Digite seu e-mail"
+              style={{
+                borderWidth: 1,
+                height: 50,
+              }}
+            />
+          </Box>
+          <TextInput
+            placeholder="Digite sua senha"
+            style={{
+              borderWidth: 1,
+              height: 50,
+            }}
+          />
+          <Text preset="paragraphSmall" bold color="primary" marginTop="s10">
+            Esqueci minha senha
+          </Text>
+
+          <Button title="Entrar" marginTop="s48" />
+          <Button title="Criar uma conta" marginTop="s12" preset="outline" />
+        </Box>
       </SafeAreaView>
     </ThemeProvider>
   );
