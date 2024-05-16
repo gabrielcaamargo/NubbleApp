5c9e616d2323040b2df85362ed4b819df58e567a
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-05-15 23:49:53 -0300

    feat: create signin form

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -49,6 +49,7 @@ export function TextInput({
         <Box {...$textInputContainer}>
           <RNTextInput
             ref={inputRef}
+            autoCapitalize="none"
             placeholderTextColor={colors.gray2}
             style={$textInputStyle}
             {...textInputProps}
--- src/screens/auth/LoginScreen/LoginScreen.tsx
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import {useForm, Controller} from 'react-hook-form';
 import {Text} from '../../../components/Text/Text';
 import {TextInput} from '../../../components/TextInput/TextInput';
-import {Icon} from '../../../components/Icon/Icon';
 import {Button} from '../../../components/Button/Button';
 import {Screen} from '../../../components/Screen/Screen';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../../routes/Routes';
+import {PasswordInput} from '../../../components/PasswordInput/PasswordInput';
+
+type LoginForm = {
+  email: string;
+  password: string;
+};
 
 type ScreenProps = NativeStackScreenProps<RootStackParamList, 'LoginScreen'>;
 
 export function LoginScreen({navigation}: ScreenProps) {
+  const {control, formState, handleSubmit} = useForm<LoginForm>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onChange',
+    delayError: 500,
+  });
+
+  function submitForm(event: LoginForm) {
+    console.log(event);
+  }
+
   function navigateToSignupScreen() {
     navigation.navigate('SignupScreen');
   }
@@ -27,18 +46,50 @@ export function LoginScreen({navigation}: ScreenProps) {
         Digite seu e-mail e senha para entrar
       </Text>
 
-      <TextInput
-        placeholder="Digite seu e-mail"
-        label="Email"
-        boxProps={{marginBottom: 's20'}}
+      <Controller
+        control={control}
+        name="email"
+        rules={{
+          required: 'E-mail obrigatório',
+          pattern: {
+            value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+            message: 'E-mail inválido',
+          },
+        }}
+        render={({field, fieldState}) => (
+          <TextInput
+            placeholder="Digite seu e-mail"
+            label="Email"
+            boxProps={{marginBottom: 's20'}}
+            value={field.value}
+            onChangeText={field.onChange}
+            errorMessage={fieldState.error?.message}
+          />
+        )}
       />
 
-      <TextInput
-        placeholder="Digite sua senha"
-        label="Senha"
-        RightComponent={<Icon color="gray2" name="eyeOn" />}
-        boxProps={{marginBottom: 's10'}}
+      <Controller
+        control={control}
+        name="password"
+        rules={{
+          required: 'Senha obrigtatória',
+          minLength: {
+            value: 8,
+            message: 'A senha deve ter no mínimo 8 caracteres',
+          },
+        }}
+        render={({field, fieldState}) => (
+          <PasswordInput
+            errorMessage={fieldState.error?.message}
+            value={field.value}
+            onChangeText={field.onChange}
+            label="Senha"
+            placeholder="Digita sua senha"
+            boxProps={{marginBottom: 's20'}}
+          />
+        )}
       />
+
       <Text
         preset="paragraphSmall"
         bold
@@ -47,7 +98,12 @@ export function LoginScreen({navigation}: ScreenProps) {
         Esqueci minha senha
       </Text>
 
-      <Button title="Entrar" marginTop="s48" />
+      <Button
+        disabled={!formState.isValid}
+        title="Entrar"
+        marginTop="s48"
+        onPress={handleSubmit(submitForm)}
+      />
       <Button
         onPress={navigateToSignupScreen}
         title="Criar uma conta"
