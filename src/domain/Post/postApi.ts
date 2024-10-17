b825b8a4397fd3fc5ab2ad945b8d7b948dbf8624
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-10-17 13:17:00 -0300

    feat: call post list from api

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -2,6 +2,17 @@ import {postListMock} from './postListMock';
 import {Post} from './types';
 
 async function getList(): Promise<Post[]> {
+  const response = await fetch('http://localhost:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer MQ.lVlnp7yACJFTA-q7v1-7pgivO6GgBLFflOBnzszNCegpEsRut-JTVkQQAdrt',
+    },
+  });
+
+  const data = await response.json();
+  console.log('Data:::', data);
+
   await new Promise(resolve => setTimeout(() => resolve(''), 1000));
   return postListMock;
 }
