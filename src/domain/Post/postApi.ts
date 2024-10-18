fc121c3e57a634775d39024f43becafa60ecf810
Gabriel Camargo <gcamargo.dev@gmail.com>
2024-10-17 23:42:46 -0300

    feat: implement axios

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,17 +1,9 @@
-import {PageAPI} from '@api';
+import {api, PageAPI} from '@api';
 import {PostAPI} from './postTypes';
 
 async function getList(): Promise<PageAPI<PostAPI>> {
-  const response = await fetch('http://127.0.0.1:3333/user/post', {
-    method: 'GET',
-    headers: {
-      Authorization:
-        'Bearer MQ.CUNmFrIXzyMqy88POemzVSTKj4n2NH65_IWXa5q0un1xja2fonUI6Zhrv4pC',
-    },
-  });
-
-  const data: PageAPI<PostAPI> = await response.json();
-  return data;
+  const response = await api.get<PageAPI<PostAPI>>('user/post');
+  return response.data;
 }
 
 export const postApi = {
