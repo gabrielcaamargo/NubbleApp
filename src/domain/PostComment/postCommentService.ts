9a9c91d7f8151817ee08bb484ea0fb686e3cb0e6
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-05-11 20:54:11 -0300

    feat: create post comment service

--- src/domain/PostComment/postCommentService.ts
+++ src/domain/PostComment/postCommentService.ts
@@ -22,6 +22,13 @@ async function getList(
   };
 }
 
+async function create(postId: number, message: string): Promise<PostComment> {
+  const postCommentAPI = await postCommentApi.create(postId, message);
+
+  return postCommentAdapter.toPost(postCommentAPI);
+}
+
 export const postCommentService = {
   getList,
+  create,
 };
