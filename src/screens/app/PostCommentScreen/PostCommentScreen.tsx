da5e98717e8f56415c758a6d193b8e12d9b68fad
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-05-11 20:47:53 -0300

    feat: create textmessage component

--- src/screens/app/PostCommentScreen/PostCommentScreen.tsx
+++ src/screens/app/PostCommentScreen/PostCommentScreen.tsx
@@ -3,7 +3,7 @@ import {FlatList, ListRenderItemInfo} from 'react-native';
 
 import {PostComment, usePostCommentList} from '@domain';
 
-import {Screen} from '@components';
+import {Box, Screen, TextMessage} from '@components';
 import {useAppSafeArea} from '@hooks';
 import {AppScreenProps} from '@routes';
 
@@ -21,20 +21,27 @@ export function PostCommentScreen({
   }
 
   return (
-    <Screen canGoBack title="Comentários">
-      <FlatList
-        data={list}
-        renderItem={renderItem}
-        showsVerticalScrollIndicator={false}
-        keyExtractor={item => item.id.toString()}
-        contentContainerStyle={{paddingBottom: bottom}}
-        ListFooterComponent={
-          <PostCommentBottom
-            fetchNextPage={fetchNextPage}
-            hasNextPage={hasNextPage}
-          />
-        }
-      />
+    <Screen canGoBack title="Comentários" flex={1}>
+      <Box justifyContent="space-between" flex={1}>
+        <FlatList
+          data={list}
+          renderItem={renderItem}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={item => item.id.toString()}
+          contentContainerStyle={{paddingBottom: bottom}}
+          ListFooterComponent={
+            <PostCommentBottom
+              fetchNextPage={fetchNextPage}
+              hasNextPage={hasNextPage}
+            />
+          }
+        />
+
+        <TextMessage
+          placeholder="Escreva um comentário"
+          onPressSend={() => console.log('send')}
+        />
+      </Box>
     </Screen>
   );
 }
