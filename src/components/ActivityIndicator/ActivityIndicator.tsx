881f8eca5795cf8c6d10070db92853f682ab937e
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-08-02 19:36:10 -0300

    feat: profile screen

--- src/components/ActivityIndicator/ActivityIndicator.tsx
+++ src/components/ActivityIndicator/ActivityIndicator.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import {
-  ActivityIndicator as RNActivityIndicator,
   ActivityIndicatorProps,
+  ActivityIndicator as RNActivityIndicator,
 } from 'react-native';
 
 import {useAppTheme} from '@hooks';
 import {ThemeColors} from '@theme';
 
 interface Props extends Omit<ActivityIndicatorProps, 'color'> {
-  color: ThemeColors;
+  color?: ThemeColors;
 }
-export function ActivityIndicator({color}: Props) {
+export function ActivityIndicator({color = 'primary'}: Props) {
   const {colors} = useAppTheme();
 
   return <RNActivityIndicator color={colors[color]} />;
