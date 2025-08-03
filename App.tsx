8daaef30f9cd3d7017dc8d4f94979e8b34d3f1ea
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-08-03 12:55:56 -0300

    feat: .

--- App.tsx
+++ App.tsx
@@ -4,8 +4,8 @@ if (__DEV__) {
 
 import React from 'react';
 
-import {ToastProvider} from '@service';
 import {ThemeProvider} from '@shopify/restyle';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {Toast} from '@components';
@@ -13,16 +13,18 @@ import {Toast} from '@components';
 import {Router} from './src/routes/Routes';
 import {theme} from './src/theme/theme';
 
+const queryClient = new QueryClient();
+
 function App(): JSX.Element {
   return (
-    <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        <ToastProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <ThemeProvider theme={theme}>
           <Router />
           <Toast />
-        </ToastProvider>
-      </ThemeProvider>
-    </SafeAreaProvider>
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
 
