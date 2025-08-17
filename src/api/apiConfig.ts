1cf80aebdc1b91b391f59e7beffaa51ee3d31d97
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-08-16 23:19:49 -0300

    feat: manage token

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -2,8 +2,4 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333/',
-  headers: {
-    Authorization:
-      'Bearer Mg.RP1_LZiYWjdNnLc6QZ90As3_FGYNDS4TG_0rCmoG5zX-4I4btpXb_miSaA8W',
-  },
 });
