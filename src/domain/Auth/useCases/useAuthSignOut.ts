d04a1d9367488b00655d080d8f3ec4185dae90fb
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-08-17 00:00:56 -0300

    feat: handle sign out

--- src/domain/Auth/useCases/useAuthSignOut.ts
+++ src/domain/Auth/useCases/useAuthSignOut.ts
@@ -1,11 +1,18 @@
+import {useAuthCredentials} from '@service';
 import {useMutation} from '@tanstack/react-query';
 
 import {authService} from '../authService';
 
 export function useAuthSignOut() {
+  const {removeCredentials} = useAuthCredentials();
+
   const mutation = useMutation<string, unknown, void>({
     mutationFn: authService.signOut,
     retry: false,
+    onSuccess: () => {
+      removeCredentials();
+      authService.removeToken();
+    },
   });
 
   return {
