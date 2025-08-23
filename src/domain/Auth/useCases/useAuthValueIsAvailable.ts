64e917c0f66fb8bbb43b767e5135489ef5c4e85b
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-08-23 12:01:28 -0300

    feat: useAuthValue generic

--- src/domain/Auth/useCases/useAuthValueIsAvailable.ts
+++ src/domain/Auth/useCases/useAuthValueIsAvailable.ts
@@ -5,26 +5,63 @@ import {useDebounce} from '@hooks';
 
 import {authService} from '../authService';
 
-interface Params {
-  username: string;
+interface Params<T extends {length: number}> {
+  value: T;
   enabled: boolean;
+  queryKey: QueryKeys;
+  isAvailableFn: (value: T) => Promise<boolean>;
 }
 
-export function useAuthIsUserNameAvailable({username, enabled}: Params) {
-  const debouncedUserName = useDebounce(username, 1500);
+function useAuthIsValueAvailable<T extends {length: number}>({
+  value,
+  enabled,
+  queryKey,
+  isAvailableFn,
+}: Params<T>) {
+  const debouncedValue = useDebounce(value, 1500);
 
   const {data, isFetching} = useQuery({
-    queryKey: [QueryKeys.IsUserNameAvailable, debouncedUserName],
-    queryFn: () => authService.isUserNameAvailable(debouncedUserName),
+    queryKey: [queryKey, debouncedValue],
+    queryFn: () => isAvailableFn(debouncedValue),
     retry: false,
     staleTime: 20000,
-    enabled: enabled && debouncedUserName.length > 0,
+    enabled: enabled && debouncedValue.length > 0,
   });
 
-  const isDebouncing = debouncedUserName !== username;
+  const isDebouncing = debouncedValue !== value;
 
   return {
     isFetching: isFetching || isDebouncing,
     isUnavailable: data === false,
   };
+}
+
+export function useAuthIsUserNameAvailable({
+  username,
+  enabled,
+}: {
+  username: string;
+  enabled: boolean;
+}) {
+  return useAuthIsValueAvailable({
+    value: username,
+    enabled,
+    queryKey: QueryKeys.IsUserNameAvailable,
+    isAvailableFn: authService.isUserNameAvailable,
+  });
+}
+
+export function useAuthIsEmailAvailable({
+  email,
+  enabled,
+}: {
+  email: string;
+  enabled: boolean;
+}) {
+  return useAuthIsValueAvailable({
+    value: email,
+    enabled,
+    queryKey: QueryKeys.IsEmailAvailable,
+    isAvailableFn: authService.isEmailAvailable,
+  });
 }
