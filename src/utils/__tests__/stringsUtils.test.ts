a9824cd6b4599fc5da1bd4bafae7612f834e0a25
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-08-27 20:33:37 -0300

    test: group using describe

--- src/utils/__tests__/stringsUtils.test.ts
+++ src/utils/__tests__/stringsUtils.test.ts
@@ -1,5 +1,17 @@
 import {stringsUtils} from '../stringsUtils';
 
-test('capitalizeFirstLetter', () => {
-  expect(stringsUtils.capitalizeFirstLetter('Ana maria')).toBe('Ana Maria');
+describe('stringsUtils', () => {
+  describe('capitalizeFirstLetter', () => {
+    it('should capitalize the first letter of each word', () => {
+      expect(stringsUtils.capitalizeFirstLetter('ana maria')).toBe('Ana Maria');
+      expect(stringsUtils.capitalizeFirstLetter('ANA MARIA')).toBe('Ana Maria');
+      expect(stringsUtils.capitalizeFirstLetter('aNa MaRia')).toBe('Ana Maria');
+    });
+
+    it('should remove leading trailing spaces', () => {
+      expect(stringsUtils.capitalizeFirstLetter('  ana maria  ')).toBe(
+        'Ana Maria',
+      );
+    });
+  });
 });
